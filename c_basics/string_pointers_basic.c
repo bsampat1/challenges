b1333ecf972362e202ce39b9ca3adcfe4c9fc417
bsampath <balaji.aws@gmail.com>
2016-08-14 17:50:05 -0700

    more strings.

--- c_basics/string_pointers_basic.c
+++ c_basics/string_pointers_basic.c
@@ -15,4 +15,25 @@ void main()
         p++;
         ptr++;
     }
+    char names[2][10] = {
+			    "Javier",
+                            "Messi"
+		       };
+    
+    printf("Size of array %d\n", sizeof(names)); 
+    mem_efficient_strings();
+}
+
+void mem_efficient_strings()
+{
+    char *names[] = {
+                       "Javier",
+                       "Messi"
+                   };
+    //names is a pointer to 2 dimensional array.
+    //it stores base address of two character arrays Javier and Messi
+    // names[0] -> [base add of Javier] == [location where j from Javier is stored]
+    // names[1] -> [base add of Messi] == [location where m from Messi is stored]
+    // http://stackoverflow.com/questions/37538/how-do-i-determine-the-size-of-my-array-in-c
+    printf("Size of array %d\n", sizeof(*names)); 
 }
